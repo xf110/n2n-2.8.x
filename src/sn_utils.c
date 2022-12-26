97656940c93bbf6099076260062dab0d64e4136b
lucktu <67133690@qq.com>
2022-12-26 15:53:06 +0800

    Cherry pick of PR 373

--- src/sn_utils.c
+++ src/sn_utils.c
@@ -222,6 +222,8 @@ int sn_init(n2n_sn_t *sss) {
 	sss->dhcp_addr.net_addr = ntohl(sss->dhcp_addr.net_addr);
 	sss->dhcp_addr.net_bitlen = N2N_SN_DHCP_NET_BIT_DEFAULT;
 
+    n2n_srand (n2n_seed()); /* https://github.com/ntop/n2n/pull/373/files */
+
 	return 0; /* OK */
 }
 
