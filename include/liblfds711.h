918cc081437a62c53ae6be8909a71cf00733f827
lucktu <67133690@qq.com>
2023-03-26 23:57:51 +0000

    directory adjustments

--- include/liblfds711.h
+++ include/liblfds711.h
@@ -9,22 +9,22 @@
   #pragma prefast( disable : 28113 28182 28183, "blah" )
 
   /***** includes *****/
-  #include "liblfds711/lfds711_porting_abstraction_layer_compiler.h"
-  #include "liblfds711/lfds711_porting_abstraction_layer_operating_system.h"
-  #include "liblfds711/lfds711_porting_abstraction_layer_processor.h"
+  #include "../thirdparty/liblfds711/inc/liblfds711/lfds711_porting_abstraction_layer_compiler.h"
+  #include "../thirdparty/liblfds711/inc/liblfds711/lfds711_porting_abstraction_layer_operating_system.h"
+  #include "../thirdparty/liblfds711/inc/liblfds711/lfds711_porting_abstraction_layer_processor.h"
 
-  #include "liblfds711/lfds711_prng.h"                                       // TRD : misc requires prng
-  #include "liblfds711/lfds711_misc.h"                                       // TRD : everything after depends on misc
-  #include "liblfds711/lfds711_btree_addonly_unbalanced.h"                   // TRD : hash_addonly depends on btree_addonly_unbalanced
-  #include "liblfds711/lfds711_freelist.h"
-  #include "liblfds711/lfds711_hash_addonly.h"
-  #include "liblfds711/lfds711_list_addonly_singlylinked_ordered.h"
-  #include "liblfds711/lfds711_list_addonly_singlylinked_unordered.h"
-  #include "liblfds711/lfds711_queue_bounded_manyproducer_manyconsumer.h"
-  #include "liblfds711/lfds711_queue_bounded_singleproducer_singleconsumer.h"
-  #include "liblfds711/lfds711_queue_unbounded_manyproducer_manyconsumer.h"
-  #include "liblfds711/lfds711_ringbuffer.h"
-  #include "liblfds711/lfds711_stack.h"
+  #include "../thirdparty/liblfds711/inc/liblfds711/lfds711_prng.h"                                       // TRD : misc requires prng
+  #include "../thirdparty/liblfds711/inc/liblfds711/lfds711_misc.h"                                       // TRD : everything after depends on misc
+  #include "../thirdparty/liblfds711/inc/liblfds711/lfds711_btree_addonly_unbalanced.h"                   // TRD : hash_addonly depends on btree_addonly_unbalanced
+  #include "../thirdparty/liblfds711/inc/liblfds711/lfds711_freelist.h"
+  #include "../thirdparty/liblfds711/inc/liblfds711/lfds711_hash_addonly.h"
+  #include "../thirdparty/liblfds711/inc/liblfds711/lfds711_list_addonly_singlylinked_ordered.h"
+  #include "../thirdparty/liblfds711/inc/liblfds711/lfds711_list_addonly_singlylinked_unordered.h"
+  #include "../thirdparty/liblfds711/inc/liblfds711/lfds711_queue_bounded_manyproducer_manyconsumer.h"
+  #include "../thirdparty/liblfds711/inc/liblfds711/lfds711_queue_bounded_singleproducer_singleconsumer.h"
+  #include "../thirdparty/liblfds711/inc/liblfds711/lfds711_queue_unbounded_manyproducer_manyconsumer.h"
+  #include "../thirdparty/liblfds711/inc/liblfds711/lfds711_ringbuffer.h"
+  #include "../thirdparty/liblfds711/inc/liblfds711/lfds711_stack.h"
 
   /***** pragmas off *****/
   #pragma warning( pop )
--- src/lfds711/lfds711_freelist/lfds711_freelist_internal.h
+++
@@ -1,7 +0,0 @@
-/***** the library wide include file *****/
-#include "src/liblfds711_internal.h"
-
-/***** private prototypes *****/
-void lfds711_freelist_internal_push_without_ea( struct lfds711_freelist_state *fs,
-                                                struct lfds711_freelist_element *fe );
-
